2ec79445d118ee3d1c4ba0842b1491c1009c6627
Fathur Walkers <fathurwalkers44@gmail.com>
2022-02-26 23:16:04 -0800

    update

--- database/migrations/2022_02_27_065156_create_mobils_table.php
+++ database/migrations/2022_02_27_065156_create_mobils_table.php
@@ -10,6 +10,16 @@ class CreateMobilsTable extends Migration
     {
         Schema::create('mobil', function (Blueprint $table) {
             $table->id();
+
+            $table->string('mobil_merk')->nullable();
+            $table->string('mobil_kondisi')->nullable();
+            $table->string('mobil_tipe_model')->nullable();
+            $table->string('mobil_max_mil')->nullable();
+            $table->string('mobil_tahun')->nullable();
+            $table->string('mobil_jenis_transmisi')->nullable(); // AUTOMATIC / MANUAL / SEMI-AUTOMATIC
+            $table->string('mobil_jenis_body')->nullable(); // COMPACT / CONVERTIBLE / COUPLE / MVP / OFF-ROAD / LAINNYA / SEDAN / SEDANO / STATION WAGON / SUV / TRANSPORTER / VAN
+            $table->string('m')->nullable();
+
             $table->timestamps();
         });
     }
