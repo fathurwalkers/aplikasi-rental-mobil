5602652f169b092b86eef627ff8ff26585981365
Fathur Walkers <fathurwalkers44@gmail.com>
2022-02-26 22:30:42 -0800

    update

--- app/Http/Controllers/PenyewaController.php
+++ app/Http/Controllers/PenyewaController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class PenyewaController extends Controller
 {
