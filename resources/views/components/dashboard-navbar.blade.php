903026b1b799e5e36dc4422dfaac579c1a1f0de0
Fathur Walkers <fathurwalkers44@gmail.com>
2022-02-26 22:45:56 -0800

    update

--- resources/views/components/dashboard-navbar.blade.php
+++ resources/views/components/dashboard-navbar.blade.php
@@ -1,3 +1,54 @@
 <div>
-    <!-- The whole future lies in uncertainty: live immediately. - Seneca -->
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+          <a href="index.html">Stisla</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+          <a href="index.html">St</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Beranda</li>
+
+            <li class="">
+                <a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Beranda</span></a>
+            </li>
+
+            <li class="nav-item dropdown">
+              <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Kelola Akun</span></a>
+              <ul class="dropdown-menu">
+                  <li><a class="nav-link" href="index.html">Lihat Informasi Akun</a></li>
+                <li><a class="nav-link" href="index-0.html">Ubah Informasi Akun</a></li>
+              </ul>
+            </li>
+
+            <li class="menu-header">Menu</li>
+
+            <li class="nav-item dropdown">
+              <a href="#" class="nav-link has-dropdown"><i class="far fa-file-alt"></i> <span>Kelola Rental</span></a>
+              <ul class="dropdown-menu">
+                <li><a class="nav-link" href="forms-advanced-form.html">Informasi Rental</a></li>
+                <li><a class="nav-link" href="forms-editor.html">Tambah Item Rental </a></li>
+                <li><a class="nav-link" href="forms-validation.html">Informasi Penyewaan</a></li>
+                <li><a class="nav-link" href="forms-validation.html">Daftar Penyewa</a></li>
+                <li><a class="nav-link" href="forms-validation.html">Laporan</a></li>
+              </ul>
+            </li>
+
+            <li class="nav-item dropdown">
+              <a href="#" class="nav-link has-dropdown"><i class="far fa-file-alt"></i> <span>Kelola Pengguna</span></a>
+              <ul class="dropdown-menu">
+                <li><a class="nav-link" href="forms-advanced-form.html">Daftar Pengguna</a></li>
+                <li><a class="nav-link" href="forms-validation.html">Informasi</a></li>
+              </ul>
+            </li>
+
+            <li><a class="nav-link" href="credits.html"><i class="fas fa-pencil-ruler"></i> <span>Pengaturan</span></a></li>
+          </ul>
+
+          <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
+            <a href="https://getstisla.com/docs" class="btn btn-info btn-lg btn-block btn-icon-split">
+              <i class="fas fa-rocket"></i> Ke Halaman Utama
+            </a>
+          </div>
+      </aside>
 </div>
