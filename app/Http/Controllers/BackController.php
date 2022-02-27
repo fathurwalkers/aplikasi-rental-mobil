08636c2fb1cf61f6a86e50c70d43629d6f4ca431
Fathur Walkers <fathurwalkers44@gmail.com>
2022-02-27 09:32:42 -0800

    update

--- app/Http/Controllers/BackController.php
+++ app/Http/Controllers/BackController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
+use App\Models\Data;
+use App\Models\Login;
+use App\Models\Mobil;
+use App\Models\Rental;
 
 class BackController extends Controller
 {
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -20,7 +20,7 @@
 <body>
   <div id="app">
     <section class="section">
-      <div class="container mt-5">
+      <div class="container mt-2">
         <div class="row">
           <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
             <div class="login-brand">
@@ -31,12 +31,13 @@
               <div class="card-header"><h4>Login</h4></div>
 
               <div class="card-body">
-                <form method="POST" action="#" class="needs-validation" novalidate="">
+                <form method="POST" action="{{ route('post-login') }}" class="needs-validation">
+                    @csrf
                   <div class="form-group">
-                    <label for="email">Email</label>
-                    <input id="email" type="email" class="form-control" name="email" tabindex="1" required autofocus>
+                    <label for="email">Username</label>
+                    <input id="email" type="text" class="form-control" name="login_username" tabindex="1" required autofocus>
                     <div class="invalid-feedback">
-                      Please fill in your email
+                      Please fill in your Username
                     </div>
                   </div>
 
@@ -49,9 +50,9 @@
                         </a>
                       </div>
                     </div>
-                    <input id="password" type="password" class="form-control" name="password" tabindex="2" required>
+                    <input id="password" type="password" class="form-control" name="login_username" tabindex="2" required>
                     <div class="invalid-feedback">
-                      please fill in your password
+                      please fill in your Password
                     </div>
                   </div>
 
@@ -68,25 +69,10 @@
                     </button>
                   </div>
                 </form>
-                <div class="text-center mt-4 mb-3">
-                  <div class="text-job text-muted">Login With Social</div>
-                </div>
-                <div class="row sm-gutters">
-                  <div class="col-6">
-                    <a class="btn btn-block btn-social btn-facebook">
-                      <span class="fab fa-facebook"></span> Facebook
-                    </a>
-                  </div>
-                  <div class="col-6">
-                    <a class="btn btn-block btn-social btn-twitter">
-                      <span class="fab fa-twitter"></span> Twitter
-                    </a>
-                  </div>
-                </div>
 
               </div>
             </div>
-            <div class="mt-5 text-muted text-center">
+            <div class="mt-1 text-muted text-center">
               Don't have an account? <a href="auth-register.html">Create One</a>
             </div>
             <div class="simple-footer">
