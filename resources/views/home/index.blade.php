eff396642b6e31f17c5d367f1f919c8087613035
Fathur Walkers <fathurwalkers44@gmail.com>
2022-03-09 03:03:38 -0800

    update

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -29,129 +29,130 @@
     </div>
 </div>
 
+<section id="daftar-kendaraan">
+    <div class="row">
 
-<div class="row">
-
-    @foreach ($kendaraan as $item)
-    <div class="col-md-4 col-lg-4 mb-4">
-        <div class="listing d-block  align-items-stretch">
-            <div class="listing-img h-100 mr-4">
-                <img src="{{ asset('default-img') }}/{{ $item->kendaraan_foto }}" alt="Image" class="img img-fix">
-            </div>
-            <div class="listing-contents h-100">
-                <h3>{{ $item->kendaraan_merk }}</h3>
-                <div class="rent-price">
-                    <strong>{{ "Rp " . number_format($item->kendaraan_harga_sewa,0,',','.')}} </strong><span class="mx-1">/</span>day
+        @foreach ($kendaraan as $item)
+        <div class="col-md-4 col-lg-4 mb-4">
+            <div class="listing d-block  align-items-stretch">
+                <div class="listing-img h-100 mr-4">
+                    <img src="{{ asset('default-img') }}/{{ $item->kendaraan_foto }}" alt="Image" class="img img-fix">
                 </div>
-                <div class="d-block d-md-flex mb-3 border-bottom pb-4 mb-2">
-                    <div class="listing-feature pr-4">
-                        <span class="caption">Transmisi :</span>
-                        <span class="number">{{ $item->kendaraan_jenis_transmisi }}</span>
+                <div class="listing-contents h-100">
+                    <h3>{{ $item->kendaraan_merk }}</h3>
+                    <div class="rent-price">
+                        <strong>{{ "Rp " . number_format($item->kendaraan_harga_sewa,0,',','.')}} </strong><span class="mx-1">/</span>day
                     </div>
-                    <div class="listing-feature pr-4">
-                        <span class="caption">Body :</span>
-                        <span class="number">{{ $item->kendaraan_jenis_body }}</span>
+                    <div class="d-block d-md-flex mb-3 border-bottom pb-4 mb-2">
+                        <div class="listing-feature pr-4">
+                            <span class="caption">Transmisi :</span>
+                            <span class="number">{{ $item->kendaraan_jenis_transmisi }}</span>
+                        </div>
+                        <div class="listing-feature pr-4">
+                            <span class="caption">Body :</span>
+                            <span class="number">{{ $item->kendaraan_jenis_body }}</span>
+                        </div>
+                        <div class="listing-feature pr-4">
+                            <span class="caption">Tipe :</span>
+                            <span class="number">{{ $item->kendaraan_tipe }}</span>
+                        </div>
                     </div>
-                    <div class="listing-feature pr-4">
-                        <span class="caption">Tipe :</span>
-                        <span class="number">{{ $item->kendaraan_tipe }}</span>
+                    <div>
+                        {{-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos eos at eum, voluptatem quibusdam.</p> --}}
+                        <p><a href="#" class="btn btn-primary btn-sm mb-1" data-toggle="modal" data-target="#modalpenyewaan{{ $item->id }}">Rental sekarang</a></p>
                     </div>
-                </div>
-                <div>
-                    {{-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos eos at eum, voluptatem quibusdam.</p> --}}
-                    <p><a href="#" class="btn btn-primary btn-sm mb-1" data-toggle="modal" data-target="#modalpenyewaan{{ $item->id }}">Rental sekarang</a></p>
                 </div>
             </div>
         </div>
-    </div>
 
-    {{-- MODAL PENYEWAAN --}}
-    <div class="modal fade" id="modalpenyewaan{{ $item->id }}" tabindex="1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
+        {{-- MODAL PENYEWAAN --}}
+        <div class="modal fade" id="modalpenyewaan{{ $item->id }}" tabindex="1" role="dialog" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
 
-                <div class="modal-header">
-                    <h4 class="modal-title">Form Penyewaan</h4>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-
-                <div class="modal-body">Apakah anda yakin ingin menyewa kendaraan ini?</div>
-
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm-2 col-md-2 col-lg-2">
-
-                        </div>
-                        <div class="col-sm-4 col-md-4 col-lg-4">
-                            <h5 class="fix-text">Merk </h5 class="fix-text">
-                            <h5 class="fix-text">Harga Sewa </h5 class="fix-text">
-                            <h5 class="fix-text">Tipe Kendaraan </h5 class="fix-text">
-                            <h5 class="fix-text">Kondisi </h5 class="fix-text">
-                            <h5 class="fix-text">Max Mil </h5 class="fix-text">
-                            <h5 class="fix-text">Tahun </h5 class="fix-text">
-                            <h5 class="fix-text">Jenis Transmisi </h5 class="fix-text">
-                            <h5 class="fix-text">Jenis Body </h5 class="fix-text">
-                        </div>
-                        <div class="col-sm-4 col-md-4 col-lg-4">
-                            <h5 class="fix-text">: {{ $item->kendaraan_merk }} </h5 class="fix-text">
-                            <h5 class="fix-text">: {{ "Rp " . number_format($item->kendaraan_harga_sewa,0,',','.')}} </h5 class="fix-text">
-                            <h5 class="fix-text">: {{ $item->kendaraan_tipe }} </h5 class="fix-text">
-                            <h5 class="fix-text">: {{ $item->kendaraan_kondisi }} </h5 class="fix-text">
-                            <h5 class="fix-text">: {{ $item->kendaraan_max_mil }} </h5 class="fix-text">
-                            <h5 class="fix-text">: {{ $item->kendaraan_tahun }} </h5 class="fix-text">
-                            <h5 class="fix-text">: {{ $item->kendaraan_jenis_transmisi }} </h5 class="fix-text">
-                            <h5 class="fix-text">: {{ $item->kendaraan_jenis_body }} </h5 class="fix-text">
-                        </div>
-                        <div class="col-sm-2 col-md-2 col-lg-2">
-
-                        </div>
-                    </div>
-                </div>
-
-                <form action="{{ route('proses-penyewaan', $item->id) }}" method="POST" enctype="multipart/form-data">
-                    @csrf
-
-                    <div class="container border-dark">
-
-                        <div class="row">
-                            <div class="col-sm-6 col-md-6 col-lg-6">
-                                <div class="form-group">
-                                    <label for="rental_durasi">Durasi Penyewaan</label>
-                                    <input type="number" class="form-control" id="rental_durasi" placeholder="Durasi penyewaan..." name="rental_durasi">
-                                </div>
-                            </div>
-                            <div class="col-sm-6 col-md-6 col-lg-6">
-                                <div class="form-group">
-                                    <label for="rental_satuan_waktu">Satuan Waktu</label>
-                                    <select id="rental_satuan_waktu" class="form-control" name="rental_satuan_waktu">
-                                        <option selected disabled>Pilih satuan waktu...</option>
-                                        <option value="HARI">Hari</option>
-                                        <option value="BULAN">Bulan</option>
-                                        <option value="JAM">Jam</option>
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-
-                    <div class="modal-footer">
-                        <button type="button" class="btn gray btn-danger" data-dismiss="modal">Batalkan</button>
-                        <button type="submit" class="btn btn-info" >
-                            Lanjutkan Penyewaan
+                    <div class="modal-header">
+                        <h4 class="modal-title">Form Penyewaan</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
-                </form>
 
+                    <div class="modal-body">Apakah anda yakin ingin menyewa kendaraan ini?</div>
+
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-2 col-md-2 col-lg-2">
+
+                            </div>
+                            <div class="col-sm-4 col-md-4 col-lg-4">
+                                <h5 class="fix-text">Merk </h5 class="fix-text">
+                                <h5 class="fix-text">Harga Sewa </h5 class="fix-text">
+                                <h5 class="fix-text">Tipe Kendaraan </h5 class="fix-text">
+                                <h5 class="fix-text">Kondisi </h5 class="fix-text">
+                                <h5 class="fix-text">Max Mil </h5 class="fix-text">
+                                <h5 class="fix-text">Tahun </h5 class="fix-text">
+                                <h5 class="fix-text">Jenis Transmisi </h5 class="fix-text">
+                                <h5 class="fix-text">Jenis Body </h5 class="fix-text">
+                            </div>
+                            <div class="col-sm-4 col-md-4 col-lg-4">
+                                <h5 class="fix-text">: {{ $item->kendaraan_merk }} </h5 class="fix-text">
+                                <h5 class="fix-text">: {{ "Rp " . number_format($item->kendaraan_harga_sewa,0,',','.')}} </h5 class="fix-text">
+                                <h5 class="fix-text">: {{ $item->kendaraan_tipe }} </h5 class="fix-text">
+                                <h5 class="fix-text">: {{ $item->kendaraan_kondisi }} </h5 class="fix-text">
+                                <h5 class="fix-text">: {{ $item->kendaraan_max_mil }} </h5 class="fix-text">
+                                <h5 class="fix-text">: {{ $item->kendaraan_tahun }} </h5 class="fix-text">
+                                <h5 class="fix-text">: {{ $item->kendaraan_jenis_transmisi }} </h5 class="fix-text">
+                                <h5 class="fix-text">: {{ $item->kendaraan_jenis_body }} </h5 class="fix-text">
+                            </div>
+                            <div class="col-sm-2 col-md-2 col-lg-2">
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <form action="{{ route('proses-penyewaan', $item->id) }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+
+                        <div class="container border-dark">
+
+                            <div class="row">
+                                <div class="col-sm-6 col-md-6 col-lg-6">
+                                    <div class="form-group">
+                                        <label for="rental_durasi">Durasi Penyewaan</label>
+                                        <input type="number" class="form-control" id="rental_durasi" placeholder="Durasi penyewaan..." name="rental_durasi">
+                                    </div>
+                                </div>
+                                <div class="col-sm-6 col-md-6 col-lg-6">
+                                    <div class="form-group">
+                                        <label for="rental_satuan_waktu">Satuan Waktu</label>
+                                        <select id="rental_satuan_waktu" class="form-control" name="rental_satuan_waktu">
+                                            <option selected disabled>Pilih satuan waktu...</option>
+                                            <option value="HARI">Hari</option>
+                                            <option value="BULAN">Bulan</option>
+                                            <option value="JAM">Jam</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div class="modal-footer">
+                            <button type="button" class="btn gray btn-danger" data-dismiss="modal">Batalkan</button>
+                            <button type="submit" class="btn btn-info" >
+                                Lanjutkan Penyewaan
+                            </button>
+                        </div>
+                    </form>
+
+                </div>
             </div>
         </div>
+
+        @endforeach
+
     </div>
-
-    @endforeach
-
-</div>
+</section>
 
 <div class="row">
     <div class="col-sm-12 col-md-12 col-lg-12 d-flex justify-content-center">
--- resources/views/layouts/home-layout.blade.php
+++ resources/views/layouts/home-layout.blade.php
@@ -70,9 +70,9 @@
               <nav class="site-navigation text-right ml-auto d-none d-lg-block" role="navigation">
                 <ul class="site-menu main-menu js-clone-nav ml-auto ">
                   <li class="active"><a href="index.html" class="nav-link">Beranda</a></li>
-                  <li><a href="listing.html" class="nav-link">Daftar Kendaraan</a></li>
+                  <li><a href="#daftar-kendaraan" class="nav-link">Daftar Kendaraan</a></li>
                   <li><a href="testimonials.html" class="nav-link">Tentang Kami</a></li>
-                  <li><a href="about.html" class="nav-link">Kontak</a></li>
+                  <li><a href="#" class="nav-link">Kontak</a></li>
                   <li><a href="{{ route('login') }}" class="nav-link"><b>Login</b></a></li>
                 </ul>
               </nav>
