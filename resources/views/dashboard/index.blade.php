54d3154f5c949d12ebd6779480eac09caa585f21
Fathur Walkers <fathurwalkers44@gmail.com>
2022-03-13 07:04:57 -0700

    update

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -10,7 +10,7 @@
 
 @section('content-body')
 {{-- <div class="container"> --}}
-    @if($users->level == "admin")
+    @if($users->login_level == "admin")
         <div class="row">
 
             <div class="col-lg-3 col-md-3 col-sm-3 col-3">
@@ -82,42 +82,8 @@
         </div>
     @endif
 
-    @if($users->level == "customer")
-        <div class="row">
+    @if($users->login_level == "customer")
 
-            <div class="col-lg-6 col-md-6 col-sm-6 col-6">
-                <div class="card card-statistic-1">
-                    <div class="card-icon bg-success">
-                        <i class="fas fa-truck"></i>
-                    </div>
-                    <div class="card-wrap">
-                        <div class="card-header">
-                            <h4>Total Kendaraan</h4>
-                        </div>
-                        <div class="card-body">
-                            {{ $kendaraan }}
-                        </div>
-                </div>
-                </div>
-            </div>
-
-            <div class="col-lg-6 col-md-6 col-sm-6 col-6">
-                <div class="card card-statistic-1">
-                    <div class="card-icon bg-primary">
-                            <i class="fas fa-users"></i>
-                    </div>
-                    <div class="card-wrap">
-                        <div class="card-header">
-                            <h4>Total Customer</h4>
-                        </div>
-                        <div class="card-body">
-                            {{ $data }}
-                        </div>
-                </div>
-                </div>
-            </div>
-
-        </div>
     @endif
 
 {{-- </div> --}}
